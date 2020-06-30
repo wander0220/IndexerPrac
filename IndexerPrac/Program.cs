e2aa768732df4cd166946ad05ae46a0a1ba39080
wander0220 <s2018s26@e-mirim.hs.kr>
2020-06-30 09:21:59 +0900

    초기 커밋.

---
+++ IndexerPrac/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerPrac
+{
+    class Program
+    {
+        class Products
+        {
+            public int this[int i]
+            {
+                get { return i; }
+                set { Console.WriteLine(i + "번째 상품 설정"); }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Products p = new Products();
+
+            Console.WriteLine(p[0]);
+            Console.WriteLine(p[1]);
+            Console.WriteLine(p[2]);
+            p[4] = 1;
+
+        }
+    }
+}
